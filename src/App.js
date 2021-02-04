48af27a7b3664f17e7180cbad262841e0e0dc69b
Akash Verma <akash.verma@lidolearning.com>
2021-02-05 02:54:12 +0530

    Added incomming life blog top banner

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import ContactMe from './components/ContactMe';
 const App = () => {
   return (
     <div className="App">
+      <div style={{ "width": "100%", "backgroundColor": "yellow", "textAlign": "center" }}>
+        <span> !-- Life blog coming soon, checkout my portfolio for now --! </span>
+      </div>
       <MyNavbar />
       <Introduction />
       <MyCarousel />
